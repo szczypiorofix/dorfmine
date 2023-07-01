8ff00f7198714e13f7eb147feca4573b95de6973
szczypiorofix <szczypiorofix@o2.pl>
2023-07-01 11:44:48 +0200

    Add useLocalStorage hook.

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
 
-export const useLocalStorage = () => {
+export function useLocalStorage<T>( defaultValue: T, objectName: string ): [ T, () => void, () => void ] {
 
+
+    const [ state, setState ] = useState<T>( defaultValue );
+
+
+    const save = (): void => {
+
+    }
+
+
+    const load = (): void => {
+        const objStr: string = localStorage.getItem( objectName ) ?? "";
+        const obj: T = ( JSON.parse( objStr ) ?? defaultValue ) as T;
+        setState( obj );
+    }
+
+
+    return [ state, save, load ];
 }
 
